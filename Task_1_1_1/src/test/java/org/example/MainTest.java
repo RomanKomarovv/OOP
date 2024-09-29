a3de91509bd9d05268872bd60a125f5fd17b3b91
RomanKomarovv <gamepunk653@gmail.com>
2024-09-29 19:37:08 +0700

    0 tests

--- Task_1_1_1/src/test/java/org/example/MainTest.java
+++ Task_1_1_1/src/test/java/org/example/MainTest.java
@@ -5,11 +5,4 @@ import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 public class MainTest {
 
-    @Test
-    public void testReverseArray() {
-        int[] array = {5, 4, 3, 2, 1};
-        int[] expected = {1, 2, 3, 4, 5};
-        Main.heapsort(array);
-        assertArrayEquals(expected, array);
-    }
 }
