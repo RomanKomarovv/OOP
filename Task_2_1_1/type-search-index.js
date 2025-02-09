d1884999f59c3a65ae5d3942bf9c26bcf588b2b0
RomanKomarovv <RomanKomarovv@users.noreply.github.com>
2025-02-09 10:46:59 +0000

    Deploying to gh-pages from @ RomanKomarovv/OOP@9a5b96917cbcf145d425b0abddfd3dd3c47273a5 🚀

---
+++ Task_2_1_1/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.example","l":"ParallelStreamSolution"},{"p":"org.example","l":"ParallelThreadSolution"},{"p":"org.example","l":"SequentialSolution"}];updateSearchResults();
